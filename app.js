9bcea0de303215484a7a29dcdbfdceaf913ae6dd
Sheriff Adekunle Tajudeen <dnexteinstein2@gmail.com>
2022-11-19 06:54:35 +0100

    Implement file upload functionality

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const multer = require('multer');
+const exp = require('constants');
+
+
+
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//View engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+//Multer configuration
+
+//storage engine
+const storage = multer.diskStorage({
+    destination: './public/image_uploads',
+    filename: function(req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+//upload variable
+const upload = multer({
+    storage: storage
+}).single('imageUpload');
+
+
+
+app.get('/', (req, res) => {
+    return res.render('index', { msgType: ''});
+})
+
+
+//Image upload route
+app.post('/upload', upload, (req, res) => {
+    if (req.err) {
+        return res.render('index', { msg: req.err, msgType: 'error' });
+    } else {
+        return res.render('index', {msg: 'Image uploaded successfully.', msgType: 'success' });
+    }
+})
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
